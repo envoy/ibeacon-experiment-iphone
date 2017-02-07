dc4eb9fe97e322045fad3065366ec6c5dd443e5c
Fang-Pen Lin <bornstub@gmail.com>
2017-02-07 14:03:52 -0800

    Monitor region broadcasting

--- iPhoneiBeaconExp/ViewController.swift
+++ iPhoneiBeaconExp/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
+
 
 class ViewController: UIViewController {
+    var locationManager: CLLocationManager!
+    var region: CLBeaconRegion!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+
+        locationManager.requestAlwaysAuthorization()
+
+        let uuid = UUID(uuidString: "5E759524-B7F2-4F3A-81E6-73B2F9728AAB")!
+        region = CLBeaconRegion(proximityUUID: uuid, major: 1, minor: 1, identifier: "ibeacon-test.envoy.com")
+
+        locationManager.startMonitoring(for: region)
+        print("Start monitoring region \(region)")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("did enter region \(region)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("did exit region \(region)")
+    }
+}
