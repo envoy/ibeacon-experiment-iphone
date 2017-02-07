fd8318cafb7060502697ac61586af9e265e1ae20
Fang-Pen Lin <bornstub@gmail.com>
2017-02-07 13:33:01 -0800

    Init project

---
+++ iPhoneiBeaconExp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneiBeaconExp
+//
+//  Created by Fang-Pen Lin on 2/7/17.
+//  Copyright © 2017 Envoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
