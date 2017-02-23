6ca7cec686b7b1573ff54e7fe12f9b73654ce30c
Fang-Pen Lin <bornstub@gmail.com>
2017-02-23 14:16:02 -0800

    Bump build number

--- iPhoneiBeaconExp/AppDelegate.swift
+++ iPhoneiBeaconExp/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
-        
+
         logUploadingQueue = DispatchQueue(label: "log-uploading-queue.envoy.com", qos: .userInteractive)
 
         locationManager = CLLocationManager()
